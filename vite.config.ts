67ef278d79ab83515b0f5f12a18aed1e56a97120
intimabeats <intimabeats@gmail.com>
2025-02-09 18:15:20 -0300

    v1.0.3

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,33 @@ import * as Sentry from "@sentry/vite-plugin"
 import envPlugin from 'vite-plugin-environment'
 
 export default defineConfig(({ mode }) => {
-  // Carregar variáveis de ambiente
+  // Carregar variáveis de ambiente do .env
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
     plugins: [
       react(),
-      envPlugin('all'),
+      // Expondo SOMENTE as variáveis necessárias
+      envPlugin([
+        'VITE_FIREBASE_API_KEY',
+        'VITE_FIREBASE_AUTH_DOMAIN',
+        'VITE_FIREBASE_PROJECT_ID',
+        'VITE_FIREBASE_STORAGE_BUCKET',
+        'VITE_FIREBASE_MESSAGING_SENDER_ID',
+        'VITE_FIREBASE_APP_ID',
+        'VITE_FIREBASE_MEASUREMENT_ID',
+        'VITE_SENTRY_DSN',
+        'VITE_APP_ENV',
+        'SENTRY_AUTH_TOKEN'
+      ]),
       Sentry.sentryVitePlugin({
-        org: "vem-simbora",
-        project: "javascript-react",
-        authToken: env.SENTRY_AUTH_TOKEN
+        org: "vemsimbora",          // Verifique se esse é o nome correto da organização
+        project: "vemsimbora", // Verifique se esse é o nome correto do projeto
+        authToken: env.SENTRY_AUTH_TOKEN,
+        release: {
+          name: env.VITE_SENTRY_RELEASE || "default-release",
+          inject: true
+        }
       })
     ],
     server: {
@@ -27,7 +43,7 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       outDir: 'dist',
-      sourcemap: true
+      sourcemap: true,
     },
     test: {
       globals: true,
