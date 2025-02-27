d99c0b7f068a77d34bfa2ae06dd4c14843dca175
intimabeats <intimabeats@gmail.com>
2025-02-27 18:32:29 -0300

    Initial commit from your app

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-adimport { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as Sentry from "@sentry/vite-plugin"
 import envPlugin from 'vite-plugin-environment'
